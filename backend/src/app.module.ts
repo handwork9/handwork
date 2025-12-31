95c871a4ff8705714a7dc6d1204d3e9fe15533b0
handwork <bullionhead@gmail.com>
2025-12-31 14:31:29 -0800

    Disable synchronize to reduce startup DB load

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -79,9 +79,8 @@ import { ShoppingListsModule } from './shopping-lists/shopping-lists.module';
         database: configService.get('database.name'),
         ssl: configService.get('database.ssl') ? { rejectUnauthorized: false } : false,
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        // Temporarily enable synchronize to create new tables (coupons, shopping_lists, etc.)
-        // TODO: Disable this after tables are created and use migrations instead
-        synchronize: true,
+        // Disable synchronize - tables should already exist
+        synchronize: false,
         logging: configService.get('NODE_ENV') === 'development',
         retryAttempts: 10,
         retryDelay: 5000,
